f80a09dc70264f200d859f1bd190d60fa45b36e5
Souparni <118586136+Souparni26@users.noreply.github.com>
2024-06-17 12:27:59 +0530

    manager.py

--- manager.py
+++ manager.py
@@ -1,12 +1,3 @@
-'''class Queue:
-    def __init__(self):
-        self.item=[]
-    def push(self,data):
-        self.item.append(data)
-    def pop(self):
-        return self.item.pop(0)
-    def size(self):
-        return len(self.item)'''
 class Stack:
     def __init__(self):
         self.items=[]
